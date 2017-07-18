6b76c06e83cfd69321ce62eda05c16878b8ab544
JonathanDuncan <duncanjonathan@yahoo.ca>
2017-07-18 17:39:25 -0600

    Get default my documents directory, Display working directory puddle list and export page

--- EntryMerge/EntryMerge.fs
+++ EntryMerge/EntryMerge.fs
@@ -1,11 +1,13 @@
 namespace EntryMerge
 open EntryMerge.PuddleList
 open FSharp.Data
-
+open System
+open System.IO
 module Main =
         
-    let  [<Literal>] wd = 
-        "C:\Users\Jonathan\Documents\Merge"
+    let directoryNameOfMyDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+    let wd = Path.Combine (directoryNameOfMyDocuments, "Merge") 
+        //"C:\Users\Jonathan\Documents\Merge"
     let exportpage = 
         "http://signtyp.uconn.edu/signpuddle/export.php"
     let signpuddleindexurl = 
@@ -15,6 +17,9 @@ module Main =
     let main argv =
         let programName  =
            "EntryMerge"
+        printfn "Working directory is %s" wd
+        printfn "Getting puddle list from %s" signpuddleindexurl
+        printfn "Base export page is %s" exportpage
         match (allsigntyppuddles signpuddleindexurl) with
             | Ok value ->  printfn "%A" value 
             | Error errmsg -> printfn "%s" errmsg
