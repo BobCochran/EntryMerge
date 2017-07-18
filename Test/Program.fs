fb378f4907425b5853398c28a5d67ccdcf2d415e
JonathanDuncan <duncanjonathan@yahoo.ca>
2017-07-18 17:21:47 -0600

    Adding Fuchu Tests

--- Test/Program.fs
+++ Test/Program.fs
@@ -1,13 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
- 
-open Fuchu
-let tests = 
-    testList "A test group" [
-        testCase "one test" <|
-            fun _ -> Assert.Equal("2+2", 4, 2+2)
-        testCase "another test" <|
-            fun _ -> Assert.Equal("3+3", 3, 3+3)
-    ]
+﻿open Fuchu
 [<EntryPoint>]
 let main args = defaultMainThisAssembly args
--- Test/PuddleListTest.fs
+++ Test/PuddleListTest.fs
@@ -1,11 +1,21 @@
 ﻿module PuddleListTest
 open EntryMerge
+open FSharp.Data
 open Fuchu
+
+let samplehtmldocument = (EntryMerge.PuddleList.SignTypeIndex.GetSample()).Html
+let puddleslength (l: Result<(string * string)list, string>) = 
+                      match l with
+                      | Ok list -> List.length list
+                      | Error err -> 0
 [<Tests>] 
 let tests = 
-    testList "A test group" [
-        testCase "one test" <|
-            fun _ -> Assert.Equal("2+2", 4, 2+2)
-        testCase "another test" <|
-            fun _ -> Assert.Equal("3+3", 6, 3+3)
+    testList "Puddle List test group" [
+        testCase "number of puddle names " <| 
+            fun _ -> Assert.Equal("number of puddle names", 48, EntryMerge.PuddleList.puddlenames samplehtmldocument |> List.length   )
+        testCase "number of puddle ids " <| 
+            fun _ -> Assert.Equal("number of puddle ids", 48, EntryMerge.PuddleList.puddleids samplehtmldocument |> List.length  )
+        testCase "number of puddles " <| 
+            fun _ -> Assert.Equal("number of puddles", 48, EntryMerge.PuddleList.retrievepuddles samplehtmldocument 
+                     |> puddleslength )
     ]
